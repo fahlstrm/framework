395916ff919106a436033dd255203fead6e125a3
Frida <fridaahlstrom@outlook.com>
2021-05-09 09:44:40 +0100

    Without .env

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -2,11 +2,25 @@
 
 namespace App\Controller;
 
-use App\Entity\Product;
+use App\Entity\Book;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 
 class BookController extends AbstractController
 {
+
+    public function __invoke(): Response
+    {
+
+    }
+
     
+    public function createBook(Request $request): Response
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $book = new Book();
+        $book->setTitle()
+    }
+
 }
